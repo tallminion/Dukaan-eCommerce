20d3c43b70d0f823d20c2caabd4327cb7e8848af
Ganti Sai Sagar <gantisaisagar@Gantis-MacBook-Air.local>
2024-06-29 07:52:23 +0530

    first commit

---
+++ DukaanWebParent/DukaanBackEnd/src/main/java/com/dukaan/admin/DukaanBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.dukaan.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukaanBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukaanBackEndApplication.class, args);
+	}
+
+}
