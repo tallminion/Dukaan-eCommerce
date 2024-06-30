345fa8386361657a3d3a169254bbd069bb0e1ac6
Ganti Sai Sagar <gantisaisagar@Gantis-MacBook-Air.local>
2024-06-30 23:45:05 +0530

    added ussr entity

---
+++ DukaanCommon/src/main/java/com/dukaan/common/entity/User.java
@@ -0,0 +1,126 @@
+package com.dukaan.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(length = 128, nullable = false, unique = true)
+	private String email;
+	@Column(length = 64, nullable = false)
+	private String password;
+	@Column(name = "first_name", length = 45, nullable = false)
+	private String firstName;
+	@Column(name = "last_name", length = 45, nullable = false)
+	private String lastName;
+	@Column(length = 64)
+	private String photos;
+	private boolean enabled;
+
+// Many-Many relationship
+	@ManyToMany
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+//	All args constructor
+	public User(Integer id, String email, String password, String firstName, String lastName, String photos,
+			boolean enabled) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.photos = photos;
+		this.enabled = enabled;
+	}
+
+//	overloaoded constructor
+	public User(String email, String password, String firstName, String lastName) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+// no args constructor
+	public User() {
+		super();
+	}
+
+//	getters and setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhotos() {
+		return photos;
+	}
+
+	public void setPhotos(String photos) {
+		this.photos = photos;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+}
