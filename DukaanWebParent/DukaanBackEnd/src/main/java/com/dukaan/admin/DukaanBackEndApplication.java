3919775e6c7e9edb62961c071182a3f3fd07c1ea
Ganti Sai Sagar <gantisaisagar@Gantis-MacBook-Air.local>
2024-06-30 12:38:12 +0530

    added ussr entity, unit repository and unit tests

--- DukaanWebParent/DukaanBackEnd/src/main/java/com/dukaan/admin/DukaanBackEndApplication.java
+++ DukaanWebParent/DukaanBackEnd/src/main/java/com/dukaan/admin/DukaanBackEndApplication.java
@@ -2,8 +2,10 @@ package com.dukaan.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({ "com.dukaan.common.entity", "com.dukaan.admin.user" })
 public class DukaanBackEndApplication {
 
 	public static void main(String[] args) {
